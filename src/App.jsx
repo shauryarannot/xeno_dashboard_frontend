22bb595ffa78b1ce0b39c4f1cc5b6afd6ea320e1
shauryarannot <rannotshaurya@gmail.com>
2025-09-15 23:17:41 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-              <Dashboard />
-          }
-        />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </Router>
   );
